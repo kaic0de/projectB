cbbfbcdd4077a93c37fa7b797ec3e0e0b0737c33
kaic0de <80317483+kaic0de@users.noreply.github.com>
2021-03-17 13:04:25 +0100

    first push
    for testing

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace projectB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          //array of integers with array size 10
+          int[] arr0 = new int[10];
+          //array of doubles with undefined size
+          double[] arr1 = new double[]{};
+          //array of strings with data defined
+          string[] arr2 = new string[] {"1", "2"};
+          //tuple of int and string with data defined
         }
     }
 }
